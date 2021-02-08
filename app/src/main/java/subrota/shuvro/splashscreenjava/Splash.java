ff915c506dcdfb36d180b07366beccd10f79e47d
SubrotaDebnath <subrota.shuvro@gmail.com>
2021-02-08 22:56:03 +0600

    Simplify

--- app/src/main/java/subrota/shuvro/splashscreenjava/Splash.java
+++ app/src/main/java/subrota/shuvro/splashscreenjava/Splash.java
@@ -15,13 +15,21 @@ public class Splash extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(new Runnable(){
+        /*new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
                 Splash.this.startActivity(new Intent(Splash.this, MainActivity.class));
                 Splash.this.finish();
 
             }
-        }, DURATION);
+        }, DURATION);*/
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash.this, MainActivity.class));
+                finish();
+            }
+        }, 1000);
     }
 }
